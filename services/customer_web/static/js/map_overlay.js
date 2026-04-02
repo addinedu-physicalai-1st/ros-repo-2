b8774ae24f89261c8370d2d3b757a0c0c322bdce
Woolim Park <woolimi91@naver.com>
2026-04-03 00:58:13 +0900

    delete scaffold2

--- services/customer_web/static/js/map_overlay.js
+++
@@ -1,5 +0,0 @@
-// Map overlay — renders robot position on shop map
-function updateRobotPosition(posX, posY) {
-  // TODO: convert map coordinates to pixel coordinates
-  console.log('[map] robot pos:', posX, posY);
-}
--- services/customer_web/static/js/stt.js
+++
@@ -1,5 +0,0 @@
-// STT (Speech-to-Text) stub using Web Speech API
-function startSTT(onResult) {
-  // TODO: implement Web Speech API
-  console.log('[STT] start');
-}
